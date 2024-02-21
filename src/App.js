34a727fcd3aeed93d0e0988d88def782ab0ee58f
Nicholas Yong Weng Hong <nicholasyong03@gmail.com>
2024-02-21 17:19:36 +0800

    Second hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Homepage/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
-    </div>
+    <Homepage/>
   );
 }
 
---
+++ src/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+        <p>helloddjdnjdnndjnd</p>
+      
+    </div>
+  );
+}
+
+export default Homepage;
